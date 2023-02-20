cf3f8d141be58b22214aaaec748438926bf116c3
Thiago Almeida Amorim <thiago.amorim@class-solutions.com.br>
2023-02-20 14:56:24 -0300

    CRUD do Cart

--- src/controllers/CartsController.js
+++ src/controllers/CartsController.js
@@ -34,6 +34,57 @@ class CartsController {
 
     };
 
+    async updatedCart(req, res) {
+
+        try {
+            const { id } = req.params;
+            const { code, price } = req.body;
+            
+            const cart = await CartModel.findById(id);
+
+            if (!cart) {
+                return res.status(401).json({
+                    error: 'Não foi possível localizar o carrinho de compras informado'
+                });
+            };
+
+            let cartAfterUpdate = await CartModel.findByIdAndUpdate(id, { code, price }, { new: true });
+
+            return res.status(200).json(cartAfterUpdate)
+            
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: 'Internal Server Error' })
+        };
+        
+    };
+
+    async destroyCart(req, res) {
+
+        try {
+            const { id } = req.params;
+            
+            const cart = await CartModel.findById(id)
+
+            if (!cart) {
+                return res.status(401).json({
+                    error: 'Não foi possível localizar o carrinho de compras informado'
+                });
+            };
+
+            await cart.deleteOne()
+
+            return res.status(200).json({
+                message: 'Carrinho deletado com sucesso!'
+            })
+            
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: 'Internal Server Error' })
+        };
+        
+    };
+
 };
 
 export default new CartsController();
--- src/routes/CartsRoutes.js
+++ src/routes/CartsRoutes.js
@@ -5,5 +5,7 @@ import CartsController from "../controllers/CartsController.js";
 
 routes.get('/Carts', CartsController.getFullCarts);
 routes.post('/Carts', CartsController.createCarts);
+routes.put('/Carts/:id', CartsController.updatedCart);
+routes.delete('/Carts/:id', CartsController.destroyCart);
 
 export default routes;
